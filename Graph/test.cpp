053142e65762776cc14a8fa5cca777409c94617c
Mario Virdis <virdis.mario97@gmail.com>
2020-06-02 17:38:22 +0200

    Made test for Graph class

--- Graph/test.cpp
+++ Graph/test.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "Graph.h"
+#include "Generator.h"
+
+using namespace std;
 
 int main(int argc, char** argv) {
     Graph G;
 
-    G.from_file(argv[1]);
+    make_graph(10, 5, 2, "test");
+
+    cout<<"Graph.from_file(\"test.gra\"):"<<endl;
+
+    G.from_file("test.gra");
+
+    cout<<endl;
+    cout<<"Checking Created Graph:"<<endl;
+
+    cout<<G<<endl;
 
     return 0;
 }
