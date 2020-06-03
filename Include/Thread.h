d0f9f82f6f55d0c08908baa6cf8a1bd54a7c132c
Mario Virdis <virdis.mario97@gmail.com>
2020-06-03 11:23:40 +0200

    Expanded RandomVisitor's interface to needs

--- Include/Thread.h
+++ Include/Thread.h
@@ -2,9 +2,25 @@
 #define _THREAD_H_
 
 #include <pthread.h>
+#include <unordered_set>
 
 #include "Types.h"
 #include "Graph.h"
+
+/*
+*	Set to 1 to enable multiple threads to visit
+*	the graph at the same time, with equal offset
+*	value, starting from different roots.
+*	If set to 0, randomized labelling will be done
+*	by creating one thread per dimension, with
+*	sequential visits for each root node
+*
+*	Let R be num. of different roots in given DAG,
+*	then:
+*	If (PARALLEL_VISITS == 0) => Num Threads == d
+*	If (PARALLEL_VISITS == 1) => Num Threads == R*d
+*/
+#define PARALLEL_VISITS 0
 
 /*
 *	A handy class that handles a pthread_barrier
@@ -39,13 +55,13 @@ class Mutex {
 	~Mutex();
 };
 
-void randomized_visit(Node* x, uint i, Graph&);
+bool randomized_visit(Node* x, int i, Graph& G, uint& rank, unordered_set<uint>& visited);
 
 /*
 *	A handy class that lets start a thread that executes
 *	a randomized visit on graph G, starting from node x
 *	and labelling in dimension i.
-*
+*	This class should adjust accordingly to PARALLEL_VISITS
 */
 class RandomVisitor {
 	pthread_t tid;
@@ -53,6 +69,8 @@ class RandomVisitor {
 	uint offset;
 	Node* x;
 	Barrier& barrier;
+	uint& rank;
+	std::unordered_set<uint>& visited_set;
 
 	static void* start_routine(void*);
 
@@ -68,6 +86,8 @@ class RandomVisitor {
 	RandomVisitor& set_offset(uint i);
 	RandomVisitor& set_start_node(Node* x);
 	RandomVisitor& set_barrier(Barrier& barrier);
+	RandomVisitor& set_visited_set(std::unordered_set<uint>& set);
+	RandomVisitor& set_rank(uint& rank);
 
 	~RandomVisitor();
 };
