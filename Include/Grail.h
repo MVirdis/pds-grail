b2a553aede6cb4f94f1721393cc77c6491a89123
Mario Virdis <virdis.mario97@gmail.com>
2020-06-03 15:45:06 +0200

    Created Grail.h

---
+++ Include/Grail.h
@@ -0,0 +1,27 @@
+#ifndef _GRAIL_H_
+#define _GRAIL_H_
+
+#include <unordered_set>
+
+#include "Graph.h"
+
+/*
+*	Set to 1 to enable multiple threads to visit
+*	the graph at the same time, with equal offset
+*	value, starting from different roots.
+*	If set to 0, randomized labelling will be done
+*	by creating one thread per dimension, with
+*	sequential visits for each root node
+*
+*	Let R be num. of different roots in given DAG,
+*	then:
+*	If (PARALLEL_VISITS == 0) => Num Threads == d
+*	If (PARALLEL_VISITS == 1) => Num Threads == R*d
+*/
+#define PARALLEL_VISITS 0
+
+bool randomized_labelling(Graph&, const uint);
+
+bool randomized_visit(Node*, int, Graph&, uint& , std::unordered_set<uint>&);
+
+#endif
--- Include/Thread.h
+++ Include/Thread.h
@@ -8,21 +8,6 @@
 #include "Graph.h"
 
 /*
-*	Set to 1 to enable multiple threads to visit
-*	the graph at the same time, with equal offset
-*	value, starting from different roots.
-*	If set to 0, randomized labelling will be done
-*	by creating one thread per dimension, with
-*	sequential visits for each root node
-*
-*	Let R be num. of different roots in given DAG,
-*	then:
-*	If (PARALLEL_VISITS == 0) => Num Threads == d
-*	If (PARALLEL_VISITS == 1) => Num Threads == R*d
-*/
-#define PARALLEL_VISITS 0
-
-/*
 *	A handy class that handles a pthread_barrier
 *
 */
@@ -54,8 +39,6 @@ class Mutex {
 
 	~Mutex();
 };
-
-bool randomized_visit(Node* x, int i, Graph& G, uint& rank, unordered_set<uint>& visited);
 
 /*
 *	A handy class that lets start a thread that executes
