5a4664f5d34e1a2f04952bc8bd528ff1654f97bf
Mario Virdis <virdis.mario97@gmail.com>
2020-06-07 11:05:49 +0200

    [Node] Changed header comments

--- Include/Node.h
+++ Include/Node.h
@@ -10,17 +10,9 @@
 /*
 *	A node of the directed Graph.
 *	It has a unique id, and points to all reachable nodes.
-*	It also stores a vector of intervals used for the DAG index.
 *
 *	Nodes are all handled through pointers because they are allocated
 *	into heap.
-*
-*	-add_interval takes also an index and this is important because
-*		when different threads will be adding intervals in an out of
-*		order fashion, we must ensure that each interval keeps the
-*		appropriate position inside the labels array. It's not just a
-*		simple push back.
-*
 */
 class Node {
 	uint id;						// Node number
