abbd69d35987193e133b114bc74cf39388e4304f
Mario Virdis <virdis.mario97@gmail.com>
2020-06-08 23:39:24 +0200

    Safer Index pointer handling

--- Index/Index.cpp
+++ Index/Index.cpp
@@ -13,6 +13,7 @@ Index& Index::set_size(uint size) {
 }
 
 Index& Index::set_interval(Interval& label, uint node_id) {
+	if (this->labels == NULL) return *this;
 	this->labels[node_id] = label;
 	return *this;
 }
@@ -22,9 +23,13 @@ uint Index::get_size(void) const {
 }
 
 Interval Index::get_interval(uint node_id) const {
+	if (this->labels == NULL) return Interval();
 	return this->labels[node_id];
 }
 
 Index::~Index() {
-	delete this->labels;
+	if(this->labels) {
+		delete[] this->labels;
+		this->labels = NULL;
+	}
 }	
